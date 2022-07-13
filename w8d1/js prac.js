5f579bfcbf597341412d39c593c0f6a595abc62a
Jacob Roger-Gordon <jrogergordon@gmail.com>
2022-07-12 20:57:31 -0400

    JS first steps

---
+++ w8d1/js prac.js
@@ -0,0 +1,49 @@
+function mysteryScoping1() {
+    const x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping2() {
+    var x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping3() {
+    const x = 'out of block';
+    if (true) {
+        const x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping4() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
+
+function mysteryScoping5() {
+    const x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    let s = 'out of block again';
+    console.log(x);
+}
+
+mysteryScoping5()
+
+
